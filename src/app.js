7c4ff5cce2c01e34f265afafc10b76882d3fe6ea
JenaPartha <ps4486105@gmail.com>
2024-01-16 23:38:38 +0530

    uytr

--- src/app.js
+++ src/app.js
@@ -1,28 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const connectDB = require('./config/database');
-const userRoutes = require('./routes/user.routes');
-const cors = require('cors');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const connectDB = require('./config/database');
+// const userRoutes = require('./routes/user.routes');
+// const cors = require('cors');
 
+// const app = express();
+// const port = process.env.PORT || 3001;
+
+// // Use body-parser middleware to parse JSON data
+// app.use(bodyParser.json());
+// app.use(cors({
+//   origin: 'http://localhost:3000', // Replace with your specific domain
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true, // Allow sending cookies
+// }));
+
+// // Connect to MongoDB
+// // connectDB();
+
+// // Use user routes
+// app.use('/api', userRoutes);
+
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+// module.exports = app;
+
+const express = require('express');
 const app = express();
+
 const port = process.env.PORT || 3001;
 
-// Use body-parser middleware to parse JSON data
-app.use(bodyParser.json());
-app.use(cors({
-  origin: 'http://localhost:3000', // Replace with your specific domain
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Allow sending cookies
-}));
-
-// Connect to MongoDB
-connectDB();
-
-// Use user routes
-app.use('/api', userRoutes);
+app.get('/', (req, res) => {
+  res.send('Hello from Render!');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
-module.exports = app;
