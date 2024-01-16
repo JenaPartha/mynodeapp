07a49cb73fe8d93c67b4b74c9c5168d90ab8ea0a
JenaPartha <ps4486105@gmail.com>
2024-01-16 23:21:33 +0530

    jhg

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        mongoose.connect('mongodb://localhost:27017/userDb', { useNewUrlParser: true, useUnifiedTopology: true });
+        mongoose.connect('mongodb://192.168.0.102:27017/userDb', { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDb', error);
