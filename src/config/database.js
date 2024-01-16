7c02193ee8f8063244c0b920f0bb148c9e822166
JenaPartha <ps4486105@gmail.com>
2024-01-16 23:35:41 +0530

    sdfsdf

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    try {
-        mongoose.connect('mongodb://192.168.0.102:27017/userDb', { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDb', error);
-    }
-}
+// const connectDB = async () => {
+//     try {
+//         mongoose.connect('mongodb://192.168.0.102:27017/userDb', { useNewUrlParser: true, useUnifiedTopology: true });
+//         console.log('Connected to MongoDB');
+//     } catch (error) {
+//         console.error('Error connecting to MongoDb', error);
+//     }
+// }
 
-module.exports = connectDB;
+// module.exports = connectDB;
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,40 +1,40 @@
-const User = require('../models/user.model');
-const Joi = require('joi');
+// const User = require('../models/user.model');
+// const Joi = require('joi');
 
-const userShema = Joi.object({
-  username: Joi.string().alphanum().min(3).max(300).required(),
-  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-})
+// const userShema = Joi.object({
+//   username: Joi.string().alphanum().min(3).max(300).required(),
+//   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+// })
 
-const createUser = async (req, res) => {
-  try {
-    const newUser = new User({
-      username: req.body.username,
-      password: req.body.password,
-    });
+// const createUser = async (req, res) => {
+//   try {
+//     const newUser = new User({
+//       username: req.body.username,
+//       password: req.body.password,
+//     });
 
-    const {error, value } = userShema.validate(req.body);
+//     const {error, value } = userShema.validate(req.body);
 
     
-    if (error) {
-      return res.status(400).json({ error: error.details[0].message });
-    }
-    const savedUser = await newUser.save();
-    res.json(savedUser);
-  } catch (error) {
-    console.error('Error saving user:', error);
-    res.status(500).send('Internal Server Error');
-  }
-};
+//     if (error) {
+//       return res.status(400).json({ error: error.details[0].message });
+//     }
+//     const savedUser = await newUser.save();
+//     res.json(savedUser);
+//   } catch (error) {
+//     console.error('Error saving user:', error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// };
 
-const getAllUsers = async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.json(users);
-  } catch (error) {
-    console.error('Error fetching users:', error);
-    res.status(500).send('Internal Server Error');
-  }
-};
+// const getAllUsers = async (req, res) => {
+//   try {
+//     const users = await User.find({});
+//     res.json(users);
+//   } catch (error) {
+//     console.error('Error fetching users:', error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// };
 
-module.exports = { createUser, getAllUsers };
+// module.exports = { createUser, getAllUsers };
