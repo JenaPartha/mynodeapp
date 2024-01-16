25391aeda98f46fb685250fbb99211b7533aa854
JenaPartha <ps4486105@gmail.com>
2024-01-16 23:28:21 +0530

    hgfty

--- test/app.js
+++ test/app.js
@@ -1,22 +1,37 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const connectDB = require('./database');
+// const userRoutes = require('./user.routes');
+
+// const app = express();
+// const port = 3000;
+
+// // Use body-parser middleware to parse JSON data
+// app.use(bodyParser.json());
+
+// // Connect to MongoDB
+// connectDB();
+
+// // Use user routes
+// app.use('/api', userRoutes);
+
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+// module.exports = app;
+
 const express = require('express');
-const bodyParser = require('body-parser');
-const connectDB = require('./database');
-const userRoutes = require('./user.routes');
-
 const app = express();
-const port = 3000;
 
-// Use body-parser middleware to parse JSON data
-app.use(bodyParser.json());
+const port = process.env.PORT || 3001;
 
-// Connect to MongoDB
-connectDB();
-
-// Use user routes
-app.use('/api', userRoutes);
+app.get('/', (req, res) => {
+  res.send('Hello from Render!');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-module.exports = app;
+
